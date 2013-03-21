f6d1b7c6651b5b6dd2ee7c5c021905050248a4ea
Maia Grotepass <maiatoday@gmail.com>
2013-03-21 12:00:58 +0200

    switch to stackTrace project and make eclipse settings

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 #cd /home/maia/oF/openFrameworks/apps/myApps/forkmerge/boodler
 #boodler --output pulse --listen --extern theagents com.example.theagents/Example &
 #cd /home/maia/oF/openFrameworks/apps/myApps/forkmerge/bin
-cd /home/maia/oF/of_v0071_linux64_release/apps/myApps/forkmerge/bin
-./forkmerge_debug
+cd /home/maia/oF/openFrameworks/apps/myApps/stackTrace/bin
+./stackTrace
